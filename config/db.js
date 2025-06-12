f76632ec832114960f5d46326f792466012d72b5
SebasTechWolf <162742889+SebasTechWolf@users.noreply.github.com>
2025-06-12 03:29:04 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const mysql = require("mysql2/promise");
 
-port: process.env.DB_PORT;
 const dbConfig = {
   host: process.env.DB_HOS,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
